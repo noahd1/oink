9e74bf695d4a4cdf1b0bc522a1c292aec2c76c92
Noah Davis <noahd1@yahoo.com>
2009-02-15 02:04:19 -0500

    fixing paths for benchmarkers

--- benchmark/oink.rb
+++ benchmark/oink.rb
@@ -2,12 +2,12 @@
 
 require 'benchmark'
 
-require File.expand_path(File.dirname(__FILE__) + "/../lib/oink")
+require File.expand_path(File.dirname(__FILE__) + "/../lib/oink_for_memory")
 
 Benchmark.bmbm(15) do |x|
   x.report("Running Oink") { 
     f = File.open(File.expand_path(File.dirname(__FILE__) + "/../logs/production.log"))
-    Oink.new([f], 75*1024).each_line do |line|
+    OinkForMemory.new([f], 75*1024).each_line do |line|
     end
     f.close
   }
