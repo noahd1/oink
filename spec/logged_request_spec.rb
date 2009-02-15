813c5f0b8b57a00d57359f709fb661702b60cc69
Noah Davis <noahd1@yahoo.com>
2009-02-15 02:07:39 -0500

    fixing logged request spec

--- spec/logged_request_spec.rb
+++ spec/logged_request_spec.rb
@@ -4,9 +4,9 @@ describe LoggedRequest do
   
   describe "sort" do
     it "should sort by memory used" do
-      lr1 = LoggedRequest.new("Controller#Action", "February 1 10:20", 10, [])
-      lr2 = LoggedRequest.new("Controller#Action", "February 1 10:20", 5, [])
-      lr3 = LoggedRequest.new("Controller#Action", "February 1 10:20", 30, [])
+      lr1 = LoggedMemoryRequest.new("Controller#Action", "February 1 10:20", [], 10)
+      lr2 = LoggedMemoryRequest.new("Controller#Action", "February 1 10:20", [], 5)
+      lr3 = LoggedMemoryRequest.new("Controller#Action", "February 1 10:20", [], 30)
       
       [lr1, lr2, lr3].sort.should == [lr2, lr1, lr3]
     end
