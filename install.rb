5c412eb261ac55ad5fa800e82f74305a43e0043c
Noah Davis <noahd1@yahoo.com>
2009-02-21 13:12:40 -0500

    making script/oink file and creating plugin hooks to install it

--- install.rb
+++ install.rb
@@ -1,1 +1,6 @@
-# Install Hook
+#!/usr/bin/env ruby
+
+template_executable_file = File.join("application_files", "script", "oink")
+executable_file = File.expand_path("#{File.dirname(__FILE__)}/../../../script/oink")
+
+File.copy template_executable_file, executable_file
--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,4 @@
-# Uninstall hook code here
+#!/usr/bin/env ruby
+
+executable_file = File.expand_path("#{File.dirname(__FILE__)}/../../../script/oink")
+File.delete(executable_file)
