a8afe7dd8c3cb48fc55f0d924b01681e08af0280
Noah Davis <noahd1@yahoo.com>
2009-02-21 13:18:39 -0500

    updating install file to require fbtools

--- install.rb
+++ install.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
+require 'ftools'
 
 template_executable_file = File.join("application_files", "script", "oink")
 executable_file = File.expand_path("#{File.dirname(__FILE__)}/../../../script/oink")
 
 File.copy template_executable_file, executable_file
+File.chmod 0755, executable_file
