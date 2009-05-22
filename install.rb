2134432acc86a67a149b07b21a34182222c50801
Noah Davis <noahd1@yahoo.com>
2009-05-22 11:23:59 -0400

    attempted fix for install.rb to fix 'plugin not found' error

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'ftools'
 
-template_executable_file = File.join("application_files", "script", "oink")
+template_executable_file = File.join(File.dirname(__FILE__), "application_files", "script", "oink")
 executable_file = File.expand_path("#{File.dirname(__FILE__)}/../../../script/oink")
 
 File.copy template_executable_file, executable_file
