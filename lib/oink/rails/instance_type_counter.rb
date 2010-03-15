42ebb96c0b26b296bd5e9c678180ed604644d95c
Scott Taylor <scott@railsnewbie.com>
2010-03-15 03:46:22 -0400

    Improve performance of AR object allocation tracking by removing dependency on ActiveSupport callback system.
                         user     system      total        real
    40,000 empty iterations  0.000000   0.000000   0.000000 (  0.002622)
    without instance type counter - instantiating 40,000 objects  0.860000   0.020000   0.880000 (  0.879087)
    with instance type counter - instating 40,000 objects  1.090000   0.020000   1.110000 (  1.108573)

--- lib/oink/rails/instance_type_counter.rb
+++ lib/oink/rails/instance_type_counter.rb
@@ -1,5 +1,5 @@
 module Oink
-  
+
   def self.extended_active_record?
     @oink_extended_active_record
   end
@@ -7,11 +7,11 @@ module Oink
   def self.extended_active_record!
     @oink_extended_active_record = true
   end
-  
+
   module InstanceTypeCounter
     def self.included(klass)
       ActiveRecord::Base.send(:include, OinkInstanceTypeCounterInstanceMethods)
-    
+
       klass.class_eval do
         after_filter :report_instance_type_count
       end
@@ -21,7 +21,7 @@ module Oink
     end
 
     private
-    
+
       def report_instance_type_count
         report_hash = ActiveRecord::Base.instantiated_hash.merge("Total" => ActiveRecord::Base.total_objects_instantiated)
         breakdown = report_hash.sort{|a,b| b[1]<=>a[1]}.collect {|k,v| "#{k}: #{v}" }.join(" | ")
@@ -31,54 +31,56 @@ module Oink
         end
         ActiveRecord::Base.reset_instance_type_count
       end
-
   end
 
   module OinkInstanceTypeCounterInstanceMethods
-  
+
     def self.included(klass)
       klass.class_eval do
-      
+
         @@instantiated = {}
         @@total = nil
-      
+
         def self.reset_instance_type_count
           @@instantiated = {}
           @@total = nil
         end
-      
+
+        def self.increment_instance_type_count
+          @@instantiated[base_class.name] ||= 0
+          @@instantiated[base_class.name] += 1
+        end
+
         def self.instantiated_hash
           @@instantiated
         end
-        
+
         def self.total_objects_instantiated
           @@total ||= @@instantiated.inject(0) { |i, j| i + j.last }
         end
 
         unless Oink.extended_active_record?
-          if instance_methods.include?("after_initialize")
-            def after_initialize_with_oink
-              after_initialize_without_oink
-              increment_ar_count
-            end
-          
-            alias_method_chain :after_initialize, :oink
-          else
-            def after_initialize
-              increment_ar_count
+          class << self
+            alias_method :allocate_before_oink, :allocate
+
+            def allocate
+              value = allocate_before_oink
+              increment_instance_type_count
+              value
             end
           end
-          
+
+          alias_method :initialize_before_oink, :initialize
+
+          def initialize(*args, &block)
+            value = initialize_before_oink(*args, &block)
+            self.class.increment_instance_type_count
+            value
+          end
+
           Oink.extended_active_record!
         end
-      
       end
     end
-  
-    def increment_ar_count
-      @@instantiated[self.class.base_class.name] ||= 0
-      @@instantiated[self.class.base_class.name] = @@instantiated[self.class.base_class.name] + 1  
-    end
-
   end
 end
