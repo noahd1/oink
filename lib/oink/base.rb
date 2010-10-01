9074d0cf46822aff9e0b1692327f5c4477033248
Noah Davis <noahd1@yahoo.com>
2010-09-30 22:20:40 -0400

    bump version to 0.1.1

--- lib/oink/base.rb
+++ lib/oink/base.rb
@@ -2,7 +2,7 @@ module Oink
 
   class Base
 
-    VERSION = '0.1.0'
+    VERSION = '0.1.1'
     FORMATS = %w[verbose short-summary summary]
     FORMAT_ALIASES = { "v" => "verbose", "ss" => "short-summary", "s" => "summary" }
     HODEL_LOG_FORMAT_REGEX = /^(\w+ \d{2} \d{2}:\d{2}:\d{2})/
