e9576bb07ec63c17579abcd9bcd2e9c524de5d3b
Noah Davis <noah@efficiency20.com>
2011-03-12 00:21:14 -0500

    move specs to be parallel to code

--- spec/oink/middleware_spec.rb
+++ spec/oink/middleware_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 require "oink/middleware"
 require 'rack/test'
 require 'logger'
--- spec/oink/rails/memory_usage_controller_spec.rb
+++ spec/oink/rails/memory_usage_controller_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
+require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
 
 class UseMemoryLoggerApplicationController < FakeApplicationController
   include Oink::MemoryUsageLogger
