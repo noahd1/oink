836b0667dc6fd48c8f2196c7f74b590a08aa182e
Noah Davis <noah@efficiency20.com>
2011-03-13 12:05:35 -0400

    move hash_utils class to utils folder

--- lib/oink.rb
+++ lib/oink.rb
@@ -1,6 +1,6 @@
 require "oink/reports/memory_usage_reporter"
 require "oink/reports/active_record_instantiation_reporter"
-require 'oink/hash_utils'
+require 'oink/utils/hash_utils'
 require "oink/cli"
 
 if defined?(Rails)
