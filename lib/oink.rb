1374507d1d1cf9a2bfc5b9515570b609a87cd8f6
Noah Davis <noah@efficiency20.com>
2011-03-12 22:18:11 -0500

    remove adding oink's lib to load path

--- lib/oink.rb
+++ lib/oink.rb
@@ -1,5 +1,3 @@
-$:.unshift(File.dirname(__FILE__ + '.rb') + '/../lib') unless $:.include?(File.dirname(__FILE__ + '.rb') + '/../lib')
-
 require "oink/memory_usage_reporter"
 require "oink/active_record_instantiation_reporter"
 require 'oink/hash_utils'
